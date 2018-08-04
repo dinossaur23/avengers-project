ef75ea60f60e69115aee5db802c424fbb0e20be0
Wendel Rodrigues Alves Olival de Lucas <t707348@isban.com.br>
2018-08-03 21:29:44 -0300

    Login and Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AvengersComponent } from './avengers/avengers.component';
-
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'avengers', component: AvengersComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'avengers', component: AvengersComponent },
+  { path: 'login', component: AuthComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
-  });
+  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
+  exports: [ RouterModule ],
+  declarations: []
+})
 
-export class AppRoutingModule { };
+export class AppRoutingModule { }
